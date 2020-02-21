bfa121c934e18b54a32c04e65c367cd4f8bc088f
Talonss5 <vothanhnhan108@gmail.com>
2020-02-20 22:17:35 -0800

    Update Simulation.java

--- project1/src/project1/Simulation.java
+++ project1/src/project1/Simulation.java
@@ -6,6 +6,7 @@
 package project1;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -18,6 +19,22 @@ public class Simulation {
         return mRandom;
 }
 
-    
+public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Please enter the seed number: ");
+        int seed=scan.nextInt();
+        mRandom=new Random(seed);
+        
+        
+        System.out.println("Please enter the number of floor: ");
+        int numfloor=scan.nextInt();
+        
+        System.out.println("Please enter the number of elevator: ");
+        int numele=scan.nextInt();
+        
+        Building building= new Building(numele,numfloor);
+        System.out.println(building);
+        
+    }
     
 }
