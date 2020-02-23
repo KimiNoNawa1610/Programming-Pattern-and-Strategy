b4de30cda6be73da502018e834c507c9346caee5
Talonss5 <vothanhnhan108@gmail.com>
2020-02-23 00:26:02 -0800

    Update Elevator.java

--- project1/src/project1/Elevator.java
+++ project1/src/project1/Elevator.java
@@ -49,97 +49,54 @@ public class Elevator {
         return number_of_elevator;
     }
    
-    public Elevator(int ele){
+    public Elevator(int ele, Building build){
         number_of_elevator=ele;
-        current_floor=1;
+        current_floor=0;
         current_state=Current_state.IDLE_STATE;
         current_direction=Current_direction.NOT_MOVING;
+        building=build;
     }
     
     public void tick(){
-        if(getCurrentState()==Current_state.IDLE_STATE){
+        
+        if(current_state==Current_state.LOADING_PASSENGERS){
+            for( int i:building.getFloor(current_floor)){
+                passengers.add(i);
+                building.getFloor(current_floor).remove(i);
+            }
+        }
+        
+        if(current_state==Current_state.DOORS_OPENING){
+            for(int i:passengers){
+                if(i==getCurrentFloor()){
+                    current_state=Current_state.UNLOADING_PASSENGERS;
+                   
+                }
+            }
+            if(passengers.isEmpty()&& !building.getFloor(current_floor).isEmpty()){
+                current_state=Current_state.LOADING_PASSENGERS;
+                
+            }
+        }
+        
+        if(current_state==Current_state.IDLE_STATE){
             if(!passengers.isEmpty()){
-                current_state=Current_state.ACCELERATING;
-                if(!building.getFloor(getCurrentFloor()).isEmpty()){
-                    current_state=Current_state.DOORS_OPENING;
+                current_state=Current_state.ACCELERATING;}
+            if(!building.getFloor(current_floor).isEmpty()){
+                current_state=Current_state.DOORS_OPENING;
+                
             }
         }
+        
+        
+            
+        
         }
-        if(getCurrentState()==Current_state.DOORS_OPENING){
-           for(int i=0;i<getPassengers().size();i++){
-               if(getPassengers().get(i).equals(getCurrentFloor())){
-                   current_state=Current_state.UNLOADING_PASSENGERS;
-                   passengers.remove(i);
-                   current_direction=Current_direction.NOT_MOVING;  
-               } 
-           }
-        }
-        if(getCurrentDirection()==Current_direction.NOT_MOVING){
-           if(!building.getFloor(getCurrentFloor()).isEmpty()&&getPassengers().isEmpty()){
-               for(int i=0;i<building.getFloor(getCurrentFloor()).size();i++){
-                   if(building.getFloor(getCurrentFloor()).get(i)>=getCurrentFloor()){
-                       current_state=Current_state.LOADING_PASSENGERS;
-                       passengers.add(building.getFloor(getCurrentFloor()).get(i));
-               }
-               }
-           }
-        }
-        if(!building.getFloor(getCurrentFloor()).isEmpty()){
-        for(int i=0;i<getPassengers().size();i++){
-            for(int j=0;j<building.getFloor(getCurrentFloor()).size();j++){
-                if(getPassengers().get(i)<= building.getFloor(getCurrentFloor()).get(j)){
-                    current_state=Current_state.LOADING_PASSENGERS;
-                    passengers.add(building.getFloor(getCurrentFloor()).get(j));
-                }
-            }
-            }
-        }
-        if(getCurrentState()==Current_state.LOADING_PASSENGERS){
-            current_state=Current_state.DOORS_CLOSING;
-        }
-        if(getCurrentState()==Current_state.UNLOADING_PASSENGERS&&building.getFloor(getCurrentFloor()).isEmpty()){
-            current_state=Current_state.DOORS_CLOSING;
-        }
-        if(getCurrentState()==Current_state.DOORS_CLOSING){
-            if(!getPassengers().isEmpty()){
-                current_state=Current_state.ACCELERATING;
-                for(int i=0;i<getPassengers().size();i++){
-                    if(getPassengers().get(i)<getCurrentFloor()){
-                        current_direction=Current_direction.UP;
-                    }
-                    else if(getPassengers().get(i)>getCurrentFloor()){
-                        current_direction=Current_direction.DOWN;
-                    }
-                }
-            }
-            else{
-                current_state=Current_state.IDLE_STATE;
-            }
-        }
-        if(getCurrentState()==Current_state.ACCELERATING){
-           current_state=Current_state.MOVING;
-           for(int i=0;i<getPassengers().size();i++){
-               if(getPassengers().get(i)<getCurrentFloor()){
-                   current_floor=current_floor+1;
-            }
-               else if(getPassengers().get(i)>getCurrentFloor()){
-                   current_floor=current_floor-1;
-               }
-               else if(getPassengers().get(i)==current_floor+1){
-                   current_state=Current_state.DECELERATING;
-               }
-        }        
-        }
-        if(getCurrentState()==Current_state.DECELERATING){
-            current_state=Current_state.DOORS_OPENING;
-            if(getPassengers().isEmpty()){
-                current_direction=Current_direction.NOT_MOVING;
-            }
-        }       
-    }
+               
     @Override
     public String toString(){
-        return "Elevator - "+getElevator()+" - "+getCurrentState()+" - "+getCurrentDirection()+" - "+getPassengers();
+        return "Elevator "+getElevator()+" - Floor "+getCurrentFloor()+" - "+getCurrentState()+" - "
+                +getCurrentDirection()+" - Passengers "+getPassengers();
     }
 }
 
