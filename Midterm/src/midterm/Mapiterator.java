d7e7f5a23a9c03b95117328f99cd7f9bfbd60372
Talonss5 <vothanhnhan108@gmail.com>
2020-02-25 13:54:00 -0800

    Create Mapiterator.java

---
+++ Midterm/src/midterm/Mapiterator.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package midterm;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author votha
+ */
+public class Mapiterator {
+    public static void main(String[] args) {
+        TreeMap<String, String> map=new TreeMap<>();
+        map.put("Nhan","1610");
+        map.put("Tring","2512");
+        map.put("Danh","1608");
+        map.put("Huong","2605");
+        
+        System.out.println("map enhanced for loop");
+        
+        for(Map.Entry<String, String> entry:map.entrySet()){
+            System.out.println("key: "+entry.getKey()+ " value: "+entry.getValue());
+            
+        }
+        System.out.println(" ");
+        System.out.println("map iterator");
+        
+        Iterator<Map.Entry<String,String>> mapiterator=map.entrySet().iterator();
+        
+        while(mapiterator.hasNext()){
+            System.out.println(mapiterator.next());
+        }
+    }
+}
