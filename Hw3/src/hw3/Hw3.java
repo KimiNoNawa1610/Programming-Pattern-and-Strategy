939e7daf061432cc592856307793fa6fabd8e06f
Talonss5 <vothanhnhan108@gmail.com>
2020-03-12 21:36:35 -0700

    Update Hw3.java

--- Hw3/src/hw3/Hw3.java
+++ Hw3/src/hw3/Hw3.java
@@ -5,11 +5,108 @@
  */
 package hw3;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author votha
  */
 public class Hw3 {
+    
+
+    abstract class Employee{
+        
+        private String Name;
+        private int Tenure ;
+        private double wages;
+        
+        public String getName(){
+            return Name;
+        }
+        
+        public int getTenure(){
+            return Tenure;
+        }
+        
+        public double getWage(){
+            return wages;
+        }
+        
+        public void setWages(double newwages){
+            wages=newwages;
+        }
+        
+        public void setTenure(int newtenure){
+            Tenure=newtenure;
+        }
+        
+        public abstract double getWages();
+        
+        public String toString(){
+            return "I'm the employee, whee!";
+        }
+        
+}
+    class HourlyEmoloyee extends Employee{
+        
+        private int workedHour;
+        private double payrate;
+
+        @Override
+        public double getWages(){
+            return workedHour*payrate;
+        }
+        
+    }
+    
+    class SalaryEmployee extends Employee{
+        private double montlysalary;
+        
+        @Override
+        public double getWages(){
+            return montlysalary;
+        }
+        
+    }
+    
+    class CommissionEmployee extends Employee{
+        private double basesalary;
+        private double monthlysales;
+        private double commission;
+        
+        @Override
+        public double getWages(){
+            return basesalary+monthlysales+commission;
+        }
+    }
+    
+    class SuperviorEmployee extends Employee{
+        private ArrayList<Employee> Employees;
+        private double bonuspercentage;
+        
+        @Override
+        public double getWages(){
+            double maxwage=0;
+            for(Employee i:Employees){
+                if(i.getWages()>maxwage){
+                    maxwage=i.getWages();
+                }
+            }
+            return maxwage+maxwage*bonuspercentage;
+        }
+    }
+    class SeniorSalaryEmployee extends Employee{
+        
+        @Override
+        public double getWages(){
+            double bonus=1.03;
+            for(int i=0;i<this.getTenure();i++){
+                bonus*=bonus;
+            }
+            return this.getWage()*bonus;
+        }
+                
+    }
 
     /**
      * @param args the command line arguments
