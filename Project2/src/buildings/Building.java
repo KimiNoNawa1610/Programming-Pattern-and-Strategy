b6d56037c30a7b548ac3a148b47c155744e29b4b
Talonss5 <vothanhnhan108@gmail.com>
2020-03-29 20:00:57 -0700

    Create Building.java

---
+++ Project2/src/buildings/Building.java
@@ -0,0 +1,80 @@
+package cecs277.buildings;
+
+import cecs277.passengers.Passenger;
+import cecs277.Simulation;
+import cecs277.elevators.Elevator;
+import cecs277.elevators.ElevatorObserver;
+
+import java.util.*;
+
+public class Building implements ElevatorObserver, FloorObserver {
+	private List<Elevator> mElevators = new ArrayList<>();
+	private List<Floor> mFloors = new ArrayList<>();
+	private Simulation mSimulation;
+	private Queue<Integer> mWaitingFloors = new ArrayDeque<>();
+	
+	public Building(int floors, int elevatorCount, Simulation sim) {
+		mSimulation = sim;
+		
+		// Construct the floors, and observe each one.
+		for (int i = 0; i < floors; i++) {
+			Floor f = new Floor(i + 1, this);
+			f.addObserver(this);
+			mFloors.add(f);
+		}
+		
+		// Construct the elevators, and observe each one.
+		for (int i = 0; i < elevatorCount; i++) {
+			Elevator elevator = new Elevator(i + 1, this);
+			elevator.addObserver(this);
+			for (Floor f : mFloors) {
+				elevator.addObserver(f);
+			}
+			mElevators.add(elevator);
+		}
+	}
+	
+
+	// TODO: recreate your toString() here.
+	
+	
+	public int getFloorCount() {
+		return mFloors.size();
+	}
+	
+	public Floor getFloor(int floor) {
+		return mFloors.get(floor - 1);
+	}
+	
+	public Simulation getSimulation() {
+		return mSimulation;
+	}
+	
+	
+	@Override
+	public void elevatorDecelerating(Elevator elevator) {
+		// Have to implement all interface methods even if we don't use them.
+	}
+	
+	@Override
+	public void elevatorDoorsOpened(Elevator elevator) {
+		// Don't care.
+	}
+	
+	@Override
+	public void elevatorWentIdle(Elevator elevator) {
+		// TODO: if mWaitingFloors is not empty, remove the first entry from the queue and dispatch the elevator to that floor.
+	}
+	
+	@Override
+	public void elevatorArriving(Floor sender, Elevator elevator) {
+		// TODO: add the floor mWaitingFloors if it is not already in the queue.
+	}
+	
+	@Override
+	public void directionRequested(Floor floor, Elevator.Direction direction) {
+		// TODO: go through each elevator. If an elevator is idle, dispatch it to the given floor.
+		// TODO: if no elevators are idle, then add the floor number to the mWaitingFloors queue.
+		
+	}
+}
