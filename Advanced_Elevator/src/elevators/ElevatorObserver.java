25aba48e6f514321238ac80ab830f4f9991f3bf4
Talonss5 <vothanhnhan108@gmail.com>
2020-03-30 15:56:00 -0700

    Update ElevatorObserver.java

--- Advanced_Elevator/src/elevators/ElevatorObserver.java
+++ Advanced_Elevator/src/elevators/ElevatorObserver.java
@@ -1,18 +1,21 @@
-package cecs277.elevators;
+package elevators;
 
 public interface ElevatorObserver {
 	/**
 	 * Triggered when an elevator enters the DECELERATING state.
+         * @param sender
 	 */
 	void elevatorDecelerating(Elevator sender);
 	
 	/**
 	 * Triggered when an elevator enters the DOORS_OPEN state.
+         * @param sender
 	 */
 	void elevatorDoorsOpened(Elevator sender);
 	
 	/**
 	 * Triggered when an elevator enters the IDLE_STATE state.
+         * @param sender
 	 */
 	void elevatorWentIdle(Elevator sender);
 }
