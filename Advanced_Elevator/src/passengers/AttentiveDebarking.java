e7ba5144ef0a1089fa94c0b0c896458a84777543
Talonss5 <vothanhnhan108@gmail.com>
2020-04-28 20:10:06 -0700

    Create AttentiveDebarking.java

---
+++ Advanced_Elevator/src/passengers/AttentiveDebarking.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package passengers;
+
+import elevators.Elevator;
+
+/**
+ *
+ * @author votha
+ */
+public class AttentiveDebarking implements DebarkingStrategy{
+
+    @Override
+    public boolean willLeaveElevator(Passenger passenger, Elevator elevator) {
+        if(passenger.getDestination()==elevator.getCurrentFloor().getNumber()){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void departedElevator(Passenger passenger, Elevator elevator) {
+        
+    }
+    
+}
