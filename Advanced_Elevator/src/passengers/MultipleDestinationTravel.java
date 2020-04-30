037e637edfc30c2048e217f41dc61b11372e875d
Talonss5 <vothanhnhan108@gmail.com>
2020-04-29 22:34:08 -0700

    Create MultipleDestinationTravel.java

---
+++ Advanced_Elevator/src/passengers/MultipleDestinationTravel.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package passengers;
+
+import buildings.Floor;
+import elevators.Simulation;
+import events.PassengerNextDestinationEvent;
+import java.util.*;
+
+/**
+ *
+ * @author votha
+ */
+public class MultipleDestinationTravel implements TravelStrategy{
+    private List<Integer> destination;
+    private List<Long>schedule;
+    
+    public MultipleDestinationTravel(List<Integer> des, List<Long> sch){
+        destination=des;
+        schedule=sch;
+        
+    }
+    @Override
+    public int getDestination() {
+        return destination.get(0);
+    }
+
+    @Override
+    public void scheduleNextDestination(Passenger passenger, Floor currentFloor) {
+        
+        this.destination.remove(0);
+        Simulation s=currentFloor.getBuilding().getSimulation();
+        PassengerNextDestinationEvent ev=new PassengerNextDestinationEvent(s.currentTime()+this.schedule.get(0),
+                    passenger,currentFloor);
+        this.schedule.remove(0);
+        if(this.destination.size()==0){
+            destination.add(1);
+        }
+        s.scheduleEvent(ev);
+        
+    }
+    
+}
