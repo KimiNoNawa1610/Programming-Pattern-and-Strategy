06cff6852d5a3521f3b29e0af808616fb67b3a35
Talonss5 <vothanhnhan108@gmail.com>
2020-04-29 22:33:59 -0700

    Create SingleDestinationTravel.java

---
+++ Advanced_Elevator/src/passengers/SingleDestinationTravel.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package passengers;
+
+import buildings.Floor;
+import elevators.Simulation;
+import events.PassengerNextDestinationEvent;
+
+/**
+ *
+ * @author votha
+ */
+public class SingleDestinationTravel implements TravelStrategy{
+    private int destination;
+    private int schedule;
+    
+    public SingleDestinationTravel(int des, int sch){
+        destination=des;
+        schedule=sch;
+    }
+    @Override
+    public int getDestination() {
+        return destination;
+    }
+
+    @Override
+    public void scheduleNextDestination(Passenger passenger, Floor currentFloor) {
+        this.destination=1;
+        Simulation s=currentFloor.getBuilding().getSimulation();
+        PassengerNextDestinationEvent ev=new PassengerNextDestinationEvent(s.currentTime()+schedule,
+                    passenger,currentFloor);
+        s.scheduleEvent(ev);
+    }
+    
+}
