6781ff7b846a644b735ca1c8dcbeafe5b6138e71
Talonss5 <vothanhnhan108@gmail.com>
2020-05-06 22:18:16 -0700

    Update DisruptiveEmbarking.java

--- Advanced_Elevator/src/passengers/DisruptiveEmbarking.java
+++ Advanced_Elevator/src/passengers/DisruptiveEmbarking.java
@@ -12,17 +12,18 @@ import elevators.Elevator;
  * @author votha
  */
 public class DisruptiveEmbarking implements EmbarkingStrategy{
+    public DisruptiveEmbarking(){}
 
     @Override
     public void enteredElevator(Passenger passenger, Elevator elevator) {
-        if(passenger.getDestination()>elevator.getCurrentFloor().getNumber()){
+        if(passenger.getTravel().getDestination()>elevator.getCurrentFloor().getNumber()){
             for( int i=0;i<elevator.getBuilding().getFloorCount()-elevator.getCurrentFloor().getNumber();i++){
-                elevator.requestFloor(elevator.getBuilding().getFloor(passenger.getDestination()+i));
+                elevator.requestFloor(elevator.getBuilding().getFloor(passenger.getTravel().getDestination()+i));
         }
         }
-        else if(passenger.getDestination()<elevator.getCurrentFloor().getNumber()){
+        else if(passenger.getTravel().getDestination()<elevator.getCurrentFloor().getNumber()){
             for( int i=0;i<elevator.getCurrentFloor().getNumber();i++){
-                elevator.requestFloor(elevator.getBuilding().getFloor(passenger.getDestination()-i));
+                elevator.requestFloor(elevator.getBuilding().getFloor(passenger.getTravel().getDestination()-i));
                 
         }
         }
