4945b4bcac086e82090cad5b0dfba2753708a391
Talonss5 <vothanhnhan108@gmail.com>
2020-05-07 19:21:11 -0700

    Update Building.java

--- Advanced_Elevator/src/buildings/Building.java
+++ Advanced_Elevator/src/buildings/Building.java
@@ -100,19 +100,10 @@ public class Building implements ElevatorObserver, FloorObserver {
 	
 	@Override
 	public void elevatorWentIdle(Elevator elevator) {
-                //System.out.println(mWaitingFloors.size());
-		// TODO: if mWaitingFloors is not empty, remove the first entry from the queue and dispatch the elevator to that floor.
                 if(mWaitingFloors.isEmpty()!=true){
                     int FirstEntry=mWaitingFloors.peek();
-                    
-                    //System.out.println("Current Waiting Floor: "+FirstEntry);
-                    //System.out.println("Elevator Current Floor:"+ elevator.getCurrentFloor().getNumber());
-                            
-                       
                     elevator.dispatchTo(mFloors.get(FirstEntry-1));
                     mWaitingFloors.remove(FirstEntry);}
-                    //System.out.println(elevator.getObserver().size());
-                    //System.out.println("Buidling: Elevator went idle");
                     
                 }
                 
@@ -120,18 +111,12 @@ public class Building implements ElevatorObserver, FloorObserver {
 	
 	@Override
 	public void elevatorArriving(Floor sender, Elevator elevator) {
-		// TODO: add the floor mWaitingFloors if it is not already in the queue.
-                //System.out.println("Buidling: Elevator arriving");
-                //System.out.println(elevator.getObserver().size());
                 if(sender.getWaitingPassengers().isEmpty()==false){
                     if(mWaitingFloors.contains(sender)!=true){
                         mWaitingFloors.add(sender.getNumber());
                     }
                 }
-                //System.out.println("Buidling: Elevator arriving");
-                //System.out.println("Elevator direction: "+elevator.getCurrentDirection());
-                //System.out.println(elevator.getObserver().size());
-
+                
 	}
 	
 	@Override
@@ -142,7 +127,6 @@ public class Building implements ElevatorObserver, FloorObserver {
                     if(ele.isIdle()==true){
                         ele.dispatchTo(floor);
                         break;
-                        //ele.setCurrentDirection(direction);
                     }
                     
                     else{
