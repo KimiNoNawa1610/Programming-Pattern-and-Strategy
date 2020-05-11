85ff19bb21f877d709ca7c22a251ade490b85b2d
Talonss5 <vothanhnhan108@gmail.com>
2020-05-10 23:07:35 -0700

    Create Jerk.java

---
+++ Advanced_Elevator/src/passengers/Jerk.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package passengers;
+
+import elevators.Simulation;
+import java.util.Random;
+
+/**
+ *
+ * @author votha
+ */
+public class Jerk implements PassengerFactory{
+    @Override
+    public String factoryName() {
+        return "Jerk";
+    }
+
+    @Override
+    public String shortName() {
+        return"Jk";
+    }
+
+    @Override
+    public int factoryWeight() {
+        return 2;
+    }
+
+    @Override
+    public BoardingStrategy createBoardingStrategy(Simulation simulation) {
+        return new CapacityBoarding();
+    }
+
+    @Override
+    public TravelStrategy createTravelStrategy(Simulation simulation) {
+        Random ran=simulation.getRandom();
+        int destination=2+ran.nextInt(simulation.getBuilding().getFloorCount()-1);
+        long schedule=(long)(3600+ran.nextGaussian()*120);
+        return new SingleDestinationTravel(destination,schedule);
+    }
+
+    @Override
+    public EmbarkingStrategy createEmbarkingStrategy(Simulation simulation) {
+        return new DisruptiveEmbarking();
+    }
+    
+
+    @Override
+    public DebarkingStrategy createDebarkingStrategy(Simulation simulation) {
+        return new AttentiveDebarking();
+    }
+    
+}
