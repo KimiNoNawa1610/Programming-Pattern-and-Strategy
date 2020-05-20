678dfb0076605d3dd0b0a96da2adf40078b21e98
Talonss5 <vothanhnhan108@gmail.com>
2020-05-19 21:16:16 -0700

    Update ConfusedDebarking.java

--- Advanced_Elevator/src/passengers/ConfusedDebarking.java
+++ Advanced_Elevator/src/passengers/ConfusedDebarking.java
@@ -6,6 +6,7 @@
 package passengers;
 
 import elevators.Elevator;
+import logging.Logger;
 import logging.StandardOutLogger;
 
 /**
@@ -25,7 +26,8 @@ public class ConfusedDebarking implements DebarkingStrategy{
     @Override
     public void departedElevator(Passenger passenger, Elevator elevator) {
         StandardOutLogger log=new StandardOutLogger(elevator.getBuilding().getSimulation());
-        log.logString(elevator.getBuilding().getSimulation().getTime()+"s: "+passenger.getName()+
+        Logger.setInstance(log);
+        log.logString(elevator.getBuilding().getSimulation().getTime()+"s: "+passenger.getName()+" "+
                     passenger.getId()+" is leaving the building at floor 1");
     }
     
