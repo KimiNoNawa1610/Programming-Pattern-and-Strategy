034a1f7821b6bb8f919cc232c5ce4bacfebd33db
Talonss5 <vothanhnhan108@gmail.com>
2020-05-21 22:02:40 -0700

    Update DisruptiveEmbarking.java

--- Advanced_Elevator/src/passengers/DisruptiveEmbarking.java
+++ Advanced_Elevator/src/passengers/DisruptiveEmbarking.java
@@ -6,6 +6,8 @@
 package passengers;
 
 import elevators.Elevator;
+import logging.Logger;
+import logging.StandardOutLogger;
 
 /**
  *
@@ -13,19 +15,33 @@ import elevators.Elevator;
  */
 public class DisruptiveEmbarking implements EmbarkingStrategy{
     public DisruptiveEmbarking(){}
-
+    
     @Override
     public void enteredElevator(Passenger passenger, Elevator elevator) {
+        StandardOutLogger log=new StandardOutLogger(elevator.getBuilding().getSimulation());
+        Logger.setInstance(log);
         if(passenger.getTravel().getDestination()>elevator.getCurrentFloor().getNumber()){
-            for( int i=0;i<elevator.getBuilding().getFloorCount()-elevator.getCurrentFloor().getNumber();i++){
+            for( int i=0;i<elevator.getBuilding().getFloorCount()-passenger.getTravel().getDestination()+1;i++){
                 elevator.requestFloor(elevator.getBuilding().getFloor(passenger.getTravel().getDestination()+i));
+                
         }
+            
+            log.logString(elevator.getBuilding().getSimulation().getTime()+"s: "+passenger.toString()+
+                            
+                    passenger.getEmbarking().toString()+" request floor "+passenger.getTravel().getDestination()+
+                            
+                    " and everything above it on elevator "+elevator.getNumber());
         }
         else if(passenger.getTravel().getDestination()<elevator.getCurrentFloor().getNumber()){
-            for( int i=0;i<elevator.getCurrentFloor().getNumber();i++){
+            for( int i=0;i<passenger.getTravel().getDestination();i++){
                 elevator.requestFloor(elevator.getBuilding().getFloor(passenger.getTravel().getDestination()-i));
                 
         }
+            log.logString(elevator.getBuilding().getSimulation().getTime()+"s: "+passenger.toString()+
+                            
+                    passenger.getEmbarking().toString()+" request floor "+passenger.getTravel().getDestination()+
+                            
+                    " and everything below it on elevator "+elevator.getNumber());
         }
     }
     @Override
